3734d95f3739770cb888c7992319c7aa389f706d
gerrydeemo <75184455+gerrydeemo@users.noreply.github.com>
2022-06-04 23:04:10 +0100

    commited

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App ()
+{
   return (
-    <View style={styles.container}>
+    <View style={ styles.container }>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      //https://www.youtube.com/watch?v=iQ_0Fd_N3Mk
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+} );
