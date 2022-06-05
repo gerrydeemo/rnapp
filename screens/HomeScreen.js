3eb258680b37dbdd131c76f4d0455782680ab544
gerrydeemo <75184455+gerrydeemo@users.noreply.github.com>
2022-06-05 16:10:06 +0100

    a

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,49 +1,95 @@
-import { Animated, Text, View, StyleSheet, Image, ImageBackground, TouchableOpacity } from 'react-native'
-import React, { useRef, useEffect } from 'react'
+import { Animated, Text, View, StyleSheet, Image, ImageBackground, TouchableOpacity, Easing } from 'react-native'
+import React, { useRef, useEffect, useState } from 'react'
 import ORBIT from "../assets/orbit.gif"
 
 
 export default function HomeScreen ()
 {
-    const fadeAnim = useRef( new Animated.Value( 0 ) ).current;
-    const fadeIn = () =>
+    const [ spinValue ] = useState( new Animated.Value( 0 ) );
+    const StyledAnimatedImage = Animated.createAnimatedComponent( ImageBackground );
+    const StylesAnimatedText = Animated.createAnimatedComponent( Text );
+    //loop an animation forever with value of spinvalue
+    useEffect( () =>
     {
-        // Will change fadeAnim value to 1 in 5 seconds
-        Animated.timing( fadeAnim, {
+        Animated.loop( Animated.timing( spinValue, {
             toValue: 1,
-            duration: 5000
-        } ).start();
-    };
-    const fadeOut = () =>
+            duration: 10050,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        } ) ).start();
+
+    }, [] );
+    const sizeUp = () =>
     {
-        // Will change fadeAnim value to 1 in 5 seconds
-        Animated.timing( fadeAnim, {
-            toValue: 0,
-            duration: 5000
-        } ).start();
-    };
+
+
+    }
+
     return (
         <View style={ styles.container }>
 
             <ImageBackground source={ require( "../assets/stars.jpg" ) } resizeMode="cover" style={ styles.bgImage }>
                 <View style={ styles.orbit }>
 
-                    <Animated.Text style={ [ styles.text, { translationX: fadeAnim } ] }>Fading View!</Animated.Text>
+                    <StyledAnimatedImage
+                        source={ require( "../assets/reactmoon.png" ) }
+                        style={
+                            [
+                                styles.moon,
+                                {
 
-                    <TouchableOpacity onPress={ fadeIn }>
+                                    transform: [ {
+                                        rotate: spinValue.interpolate( {
+                                            inputRange: [ 0, 1 ],
+                                            outputRange: [ "0deg", "360deg" ]
+                                        } )
+                                    },
+                                    { perspective: 1000 } ]
+
+                                } ] } />
+
+                    <StyledAnimatedImage
+                        source={ require( "../assets/expressstar.png" ) }
+                        style={
+                            [
+                                styles.moon,
+                                {
+
+                                    transform: [ {
+                                        rotate: spinValue.interpolate( {
+                                            inputRange: [ 0, 1 ],
+                                            outputRange: [ "0deg", "-360deg" ]
+                                        } )
+                                    },
+                                    { perspective: 1000 } ]
+
+                                } ] } ><StylesAnimatedText style={
+                                    [
+                                        styles.expressText,
+                                        {
+
+                                            transform: [ {
+                                                rotate: spinValue.interpolate( {
+                                                    inputRange: [ 0, 1 ],
+                                                    outputRange: [ "0deg", "360deg" ]
+                                                } )
+                                            },
+                                            { perspective: 1000 } ]
+
+                                        } ] }>EXPRESS</StylesAnimatedText></StyledAnimatedImage>
+                    <TouchableOpacity onPress={ sizeUp }>
                         <Image
                             source={ ORBIT }
                             style={ styles.earthImage }
                         />
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={ fadeOut }>
-                        <Image
-                            source={ ORBIT }
-                            style={ styles.earthImage }
-                        />
-                    </TouchableOpacity>
+
+
+
                 </View>
+
             </ImageBackground>
+
         </View>
     )
 
@@ -54,24 +100,36 @@ const styles = StyleSheet.create( {
         flex: 1,
         backgroundColor: '#000',
     },
-    text: {
-        textAlign: 'center',
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold'
+
+    moon: {
+        width: "80%",
+        height: "80%",
+        position: 'absolute',
+
+        zIndex: 1,
     },
 
+
+    expressText: {
+        color: 'black',
+        fontSize: 10,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 100,
+    },
     bgImage: {
         flex: 1,
         justifyContent: "center"
     },
     orbit: {
         flex: 1,
+        width: '100%',
+        height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
     },
     earthImage: {
-        width: 200,
-        height: 200,
+        width: 120,
+        height: 120,
     },
 } )
